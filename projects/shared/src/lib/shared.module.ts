25b268f70629ed5c800efd856e2241575171275f
Mayank Jaiswal <jaiswalmayank450@gmail.com>
2024-07-06 17:46:11 +0530

    Moved Homepage component to lib

--- projects/shared/src/lib/shared.module.ts
+++ projects/shared/src/lib/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { SharedComponent } from './shared.component';
-import { HomepageComponent } from '../../homepage/homepage.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 
