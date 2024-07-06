cd6916d34177c4dd4c7b492e711c0564aa2fdb9b
Mayank Jaiswal <jaiswalmayank450@gmail.com>
2024-07-06 17:21:02 +0530

    Created shared library and homepage component and registered on shared module

---
+++ projects/shared/src/lib/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { SharedComponent } from './shared.component';
+import { HomepageComponent } from '../../homepage/homepage.component';
+
+
+
+@NgModule({
+  declarations: [
+    SharedComponent,
+    HomepageComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    SharedComponent
+  ]
+})
+export class SharedModule { }
